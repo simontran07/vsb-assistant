688b3e471b9f626074c65b0a9e4b72f6f29aee78
Simon Tran <simontran105@gmail.com>
2023-12-21 13:20:39 -0500

    Quick Swing edit

---
+++ VsbAssistant.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class VsbAssistant {
+    private JFrame frame = new JFrame();
+
+    public VsbAssistant() {
+    }
+
+    public static void main(String[] args) {
+        new VsbAssistant();
+    }
+}
